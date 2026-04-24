a89de6be5ed53081cbc3adf32f40596114d01c2d
co12bg-design <co12bg@gmail.com>
2026-04-24 15:35:06 -0500

    Terminado

---
+++ script.js
@@ -0,0 +1,27 @@
+/* Muestra mensaje de bienvenida al cargar la página */
+function bienvenida() {
+    alert("¡Bienbenido a mi sitio interactivo!");
+}
+/*
+// Selección de elementos del DOM por su ID*/
+const parrafo = document.getElementById('parrafoTexto');
+const btnTexto = document.getElementById('btnCambiarTexto');
+const btnToggle = document.getElementById('btnToggle');
+const contenidoExtra = document.getElementById('contenidoExtra');
+
+ /* Al hacer clic, actualiza el contenido del párrafo */
+btnTexto.addEventListener('click', () => {
+    parrafo.innerText = "¡Soy Rafael Ramos 21 años, soy un futuro desarrollador de software en formación en Riwi, apasionado por la tecnología y la resolución de problemas. Combino mi disciplina académica con un estilo de vida activo, disfrutando del gimnasio y el fútbol, lo que me ha enseñado el valor del trabajo en equipo y la perseverancia. Me caracterizo por ser una persona de sólidos valores, comprometida con el crecimiento constante y la integridad en cada proyecto que emprendo.!";
+    parrafo.style.color = "black"; 
+})
+
+/* Muestra y oculta la seccion de contenido extra al hacer clic en el botón */
+btnToggle.addEventListener('click', () => {
+    if (contenidoExtra.style.display === "none" || contenidoExtra.style.display === "") {
+        contenidoExtra.style.display = "block";
+        btnToggle.innerText = "Ocultar";
+    } else {
+        contenidoExtra.style.display = "none";
+        btnToggle.innerText = "Mostrar";
+    }
+})
